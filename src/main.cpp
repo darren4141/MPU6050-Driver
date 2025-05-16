fccfec3257298ed7915cf6587ed18c7d497c4ef1
Darren Liu <darrrenliu@gmail.com>
2025-05-16 12:02:17 -0400

    read, write example

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,62 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define I2C_ADDR 0x68
+
+byte I2C_Scan();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin(I2C_SDA, I2C_SCL);
+  Serial.begin(115200);
+  int scanned_addr = I2C_Scan();
+
+  if(scanned_addr == 0){
+    Serial.println("No I2C Devices found");
+  }else{
+    Serial.print("I2C Device found at: ");
+    Serial.println(scanned_addr, HEX);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //aa test
+  //write raw data example:
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(0x01); //register
+  Wire.write(0xAB);
+  Wire.endTransmission();
+
+  //read raw data example:
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(0x01); //register
+  Wire.endTransmission(false); //send a restart
+
+  Wire.requestFrom(I2C_ADDR, 1); //request 1 byte
+
+  if(Wire.available()){
+    uint8_t recieved = Wire.read();
+    Serial.println(recieved, HEX);
+  }
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+byte I2C_Scan(){
+  byte error;
+  byte address;
+
+  for(address = 1; address < 127; address++){
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if(error == 0){
+      break;
+    }
+  }
+
+  if(error == 0){
+    return address;
+  }else{
+    return 0x00;
+  }
 }
