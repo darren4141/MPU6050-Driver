fc6b946fa45c6432b8d9955f3c3f1e71c3f8d4a5
Darren Liu <darrrenliu@gmail.com>
2025-05-16 18:32:18 -0400

    simple get accel values from reg

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #define I2C_SCL 22
 #define I2C_ADDR 0x68
 
+#define ACCEL_XOUT_H 0x3B //XOUT to ZOUT is 6 bytes from 0x3B to 0x40
+
 byte I2C_Scan();
+void get_accel_data(uint16_t (&xout)[3]);
 
 void setup() {
   Wire.begin(I2C_SDA, I2C_SCL);
@@ -18,27 +21,44 @@ void setup() {
     Serial.print("I2C Device found at: ");
     Serial.println(scanned_addr, HEX);
   }
+
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0x0);
+  Wire.endTransmission();
+
 }
 
 void loop() {
-  //write raw data example:
-  Wire.beginTransmission(I2C_ADDR);
-  Wire.write(0x01); //register
-  Wire.write(0xAB);
-  Wire.endTransmission();
+  // //write raw data example:
+  // Wire.beginTransmission(I2C_ADDR);
+  // Wire.write(0x01); //register
+  // Wire.write(0xAB);
+  // Wire.endTransmission();
 
-  //read raw data example:
-  Wire.beginTransmission(I2C_ADDR);
-  Wire.write(0x01); //register
-  Wire.endTransmission(false); //send a restart
+  // //read raw data example:
+  // Wire.beginTransmission(I2C_ADDR);
+  // Wire.write(0x01); //register
+  // Wire.endTransmission(false); //send a restart
 
-  Wire.requestFrom(I2C_ADDR, 1); //request 1 byte
+  // Wire.requestFrom(I2C_ADDR, 1); //request 1 byte
 
-  if(Wire.available()){
-    uint8_t recieved = Wire.read();
-    Serial.println(recieved, HEX);
+  // if(Wire.available()){
+  //   uint8_t recieved = Wire.read();
+  //   Serial.println(recieved, HEX);
+  // }
+  uint16_t xout_data[3] = {0x00, 0x00, 0x00};
+
+  get_accel_data(xout_data);
+
+  for(int i = 0; i < 3; i++){
+    Serial.print(xout_data[i]);
   }
 
+  Serial.println();
+
+  delay(100);
+  
 }
 
 byte I2C_Scan(){
@@ -58,5 +78,19 @@ byte I2C_Scan(){
     return address;
   }else{
     return 0x00;
+  }
+}
+
+void get_accel_data(uint16_t (&accel)[3]){
+
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(ACCEL_XOUT_H);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(I2C_ADDR, 6);
+  if(Wire.available()){
+    accel[0] = Wire.read() << 8 | Wire.read();
+    accel[1] = Wire.read() << 8 | Wire.read();
+    accel[2] = Wire.read() << 8 | Wire.read();
   }
 }
